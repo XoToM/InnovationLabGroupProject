e8ec6b990ba84debbc6740a14d05900631756f67
XoToM <xotom.11.23@gmail.com>
2025-04-25 19:40:37 +0100

    Added A dark theme

---
+++ MobileApp/constants/themes/dark.tsx
@@ -0,0 +1,38 @@
+import {StyleSheet} from "react-native";
+
+const TEXT_COLOR = "#FFFFFF";
+
+export default StyleSheet.create({
+	background:{
+		backgroundColor: "#000000"
+	},
+	button:{
+		borderColor:"#353535",
+		backgroundColor: "#555555",
+		color:TEXT_COLOR
+	},
+	title:{
+		color:TEXT_COLOR
+	},
+	h1:{
+		color:TEXT_COLOR
+	},
+	h2:{
+		color:TEXT_COLOR
+	},
+	h3:{
+		color:TEXT_COLOR
+	},
+	p:{
+		color:TEXT_COLOR
+	},
+	buttonText:{
+		color:TEXT_COLOR
+	},
+	toggle:{
+		color:TEXT_COLOR
+	},
+	toggleLabel:{
+		color:TEXT_COLOR
+	},
+});
