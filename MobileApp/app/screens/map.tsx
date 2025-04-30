b2ea03441ed94ba629fcdf24a29f9c9a9d51c247
XoToM <xotom.11.23@gmail.com>
2025-04-30 17:42:38 +0100

    Added location markers

--- MobileApp/app/screens/map.tsx
+++ MobileApp/app/screens/map.tsx
@@ -46,45 +46,29 @@ function LocationPreview({info}:any){
 }
 
 export default function ScreenMap() {
-	let locations1 = [
-		{name:"Park", img:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Halleyparknovember_b.jpg/800px-Halleyparknovember_b.jpg"},
-		{name:"Castle", img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"},
-		{name:"Castle", img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"},
-		{name:"Castle", img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"}
+	const locations = [
+		{name:"Park", pos:[51.37692748456796, -2.436908958136046].reverse(), img:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Halleyparknovember_b.jpg/800px-Halleyparknovember_b.jpg"},
+		{name:"Castle1",pos:[51.379306590874634, -2.354695695635474].reverse(), img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"},
+		{name:"Castle2",pos:[52.956237708377174, 4.760612002768222].reverse(), img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"},
+		{name:"Castle3",pos:[35.68616796711613, 139.75224497059952].reverse(), img:"https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/regions/sussex/places/bodiam-castle/library/winter/bodiam-castle-and-moat-in-winter-1456846.jpg"}
 		];
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			{Constants.expoConfig?.extra?.MAPBOX_ACCESS_TOKEN?<MapView style={StyleSheet.absoluteFillObject}>
-			{/*
-				<MarkerView coordinate={[0,0]}>
-					<View style={{ 
-							alignItems: 'center',
-							backgroundColor: 'red',
-							borderRadius: 30,
-							bottom: 16,
-							justifyContent: 'center',
-							left: 48,
-							minHeight: 60,
-							paddingVertical: 16,
-							position: 'absolute',
-							right: 48
-						}}>
-						<LocationPuck
-							topImage="topImage"
-							visible={true}
-							scale={['interpolate', ['linear'], ['zoom'], 10, 1.0, 20, 4.0]}
-							pulsing={{
-								isEnabled: true,
-								color: 'teal',
-								radius: 50.0,
-							}}
-						/>
-						<Text>Tap on map to add a point annotation</Text>
-					</View>
-				</MarkerView>
-						*/}
-				<Camera followUserLocation={true}/>
-			</MapView>:<Text style={StyleSheet.absoluteFillObject}>No tokens provided</Text>}
+			{Constants.expoConfig?.extra?.MAPBOX_ACCESS_TOKEN?(
+				<MapView style={StyleSheet.absoluteFillObject}>
+					
+					<Camera defaultSettings={{ centerCoordinate: [0,0], zoomLevel: 9 }}/>
+					
+					{
+						locations.map((loc,i)=>{
+							return	<MarkerView coordinate={[loc.pos[0], loc.pos[1]]}>
+										<Text>{loc.name}</Text>
+									</MarkerView>;
+						})
+							
+					}
+				</MapView>):<Text style={StyleSheet.absoluteFillObject}>No tokens provided</Text>
+			}
 			{/*<View style={[styles.mapMenu, {padding:10}]}> styleURL={StyleURL.Street}
 				<Text style={styles.header}>Featured</Text>
 				<FlatList horizontal={true} data={locations1} renderItem={(i)=><LocationPreview info={i.item}/>} ItemSeparatorComponent={() => <View style={{height:1,width: 10}} />}/>
