a32b735144ba31064f0cb02afc78a8a1c33e6261
XoToM <xotom.11.23@gmail.com>
2025-05-04 01:28:22 +0100

    Fix for links not working on the menu screen

--- MobileApp/app/screens/menu.tsx
+++ MobileApp/app/screens/menu.tsx
@@ -1,34 +1,21 @@
 
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { View, Text, Button } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 
 export default function ScreensMenu() {
+  let router = useRouter();
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text>Screens Menu</Text>
-			<Link href="/screens/login">
-				<Button title="Login" />
-			</Link>
-			<Link href="/screens/signup">
-				<Button title="Signup" />
-			</Link>
-			<Link href="/screens/map">
-				<Button title="Map" />
-			</Link>
-			<Link href="/screens/place">
-				<Button title="Place" />
-			</Link>
-			<Link href="/screens/settings">
-				<Button title="Settings" />
-			</Link>
-			<Link href="/screens/filter">
-				<Button title="Filter" />
-			</Link>
-			<Link href="/screens/places">
-				<Button title="Places" />
-			</Link>
+			<Button title="Login" onPress={()=>router.push("/screens/login")}/>
+			<Button title="Signup" onPress={()=>router.push("/screens/signup")}/>
+			<Button title="Map" onPress={()=>router.push("/screens/map")}/>
+			<Button title="Place" onPress={()=>router.push("/screens/place")}/>
+			<Button title="Settings" onPress={()=>router.push("/screens/settings")}/>
+			<Button title="Filter" onPress={()=>router.push("/screens/filter-places")}/>
+			<Button title="Places" onPress={()=>router.push("/screens/places")}/>
 		</View>
 	);
 }
