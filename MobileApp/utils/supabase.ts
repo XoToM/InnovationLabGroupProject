f7a5de65ac61bf98232dfef709a886a94d998b69
Ali-22-lab <115995633+Ali-22-lab@users.noreply.github.com>
2025-05-07 23:16:27 +0100

    trail

--- MobileApp/utils/supabase.ts
+++ MobileApp/utils/supabase.ts
@@ -1,8 +1,8 @@
 // utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://vjhazdcohumvexuluuxb.supabase.co'; // 🔁 replace this
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqaGF6ZGNvaHVtdmV4dWx1dXhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODIxODUsImV4cCI6MjA2MjE1ODE4NX0.uf4-CaBicJqxw1wiCUhDTlcSSAqQnKlndCLqCgJTUbU'; // 🔁 replace this
+const SUPABASE_URL = 'https://ssbjudrizasyltmnxfxy.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzYmp1ZHJpemFzeWx0bW54Znh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NTU0NDMsImV4cCI6MjA2MjIzMTQ0M30.pFh9fjDVm7lMJ8uSuEhvEkAebqrqwb0BUt92N5aSVw8'; 
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
